d7802e2fb17c3bb40c7863e758a8f15975ed7b91
ArghyaBanerjee30 <arghya.banerjee@thoughtworks.com>
2025-05-25 12:21:56 +0530

    Modified app.py, added logger.py, deleted utils.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-print('Hello from app')
+logger.info('Hello from app')
 
-from math import round
+print('Hello from app (updated)')
+
+from math import pi
+
+x=10+5
+print("Debug 1")
+print("Debug 2")
+if x == True:
+    print("This won't run")
+if x is False:
+    print("Also false check")
+
