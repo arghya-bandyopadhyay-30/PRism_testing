612eacf5439c3d55e787eca2dd0039ceb55f6bea
ArghyaBanerjee30 <arghya.banerjee@thoughtworks.com>
2025-05-27 02:28:30 +0530

    Update app.py with debugging logic and create logger.py for error handling

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-logger.info('Hello from app')
-
 print('Hello from app (updated)')
-<<<<<<< HEAD
 
-from math import pi
+from math import *
 
 x=10+5
 print("Debug 1")
@@ -12,8 +9,3 @@ if x == True:
     print("This won't run")
 if x is False:
     print("Also false check")
-
-=======
-print('Updated logic')
-print('Updated another logic')
->>>>>>> 5e2db8e (added new line in app.py)
---
+++ sample2.py
@@ -0,0 +1,5 @@
+def process_data():
+    try:
+        print("Processing started")
+    except Exception as e:
+        print("Something went wrong")
